5c03ede415df95a2e30c9ed290d7ae23dba61376
Flaw1331 <christian.ensz@gmail.com>
2018-06-09 04:01:57 -0500

    graphic v0.4

--- Testing_Audio/js/graphic.js
+++ Testing_Audio/js/graphic.js
@@ -18,16 +18,22 @@ function makeResponsive() {
             svgArea.remove();
         };
 
+        // Static arrays
+        var keysList = [];
+        var valueList = [];
+        var valueListUnedit = [];
 
         // SVG wrapper dimensions are determined by the current width and height of the browser window.
-        var svgWidth = window.innerWidth - 700;
+        var svgWidth = d3.select('.row').node().getBoundingClientRect().width;
         var svgHeight = window.innerHeight - 300;
 
-        console.log(Object.keys(jsonData));
-        // var keys = Object.keys(jsonData);
+
         var data = jsonData;
         Object.keys(data).forEach(function (key){
             key.value = +key.value;
+            valueListUnedit.push(data[key]);
+            valueList.push(Math.round(data[key]));
+            keysList.push(key);
         });
 
         // Create SVG wrapper
@@ -42,61 +48,73 @@ function makeResponsive() {
         // Creating tooltips
         // var toolTip = d3.tip()
         //     .attr('class', 'tooltip')
-        //     .offset([150,0])
+        //     .offset([-150,0])
         //     .html(function(data, index) {
-        //         var word = data.key;
+        //         var word = keysList[index];
         //         return (`${word}`);
         //     });
 
         // Calling Tooltips
         // chart.call(toolTip);
 
-
-        console.log(svgHeight/2, svgWidth/Object.keys(data).length);
         // Plotting circles
         chart.selectAll("circle")
-            .data(data)
+            .data(keysList)
             .enter()
             .append("circle")
-            .attr("cx", function(data) {
-                return svgWidth/Object.keys(data).length;
+            .attr("cx", function(data, index) {
+                return ((index+1)/(keysList.length+1)) * svgWidth;
             })
-            .attr("cy", function(data) {
-                return svgHeight/2;
+            .attr("cy", function(data, index) {
+                return (svgHeight/2)+(-100*(valueList[index]/1));
             })
             .attr("r", function(data) {
-                return 50;
+                return 20;
             })
-            .attr("fill", 'black')
+            .attr("fill", function(data, index) {
+                if (valueList[index] > 0)
+                    return 'green';
+                else
+                    return 'red';
+            })
+            .attr("stroke", 'black')
+            .attr("stroke-width", 2)
             .attr("opacity", ".8")
             .on("mouseover", function(data, index) {
-                d3.select(this)
-                    .transition()
-                    .duration(500)
-                    .attr("r", '60')
-                    .style("fill", "yellow");
+                if (valueList[index] > 0)
+                    d3.select(this)
+                        .transition()
+                        .duration(500)
+                        .attr("r", '60');
+                else
+                    d3.select(this)
+                        .transition()
+                        .duration(500)
+                        .attr("r", '40');
                 //toolTip.show(data);
             })
             .on("mouseout", function(data, index) {
                 d3.select(this)
                     .transition()
                     .duration(500)
-                    .attr("r", "50")
-                    .style("fill", "black");
+                    .attr("r", "20");
                 //toolTip.hide(data);
             });
-        
-        console.log(Object.keys(data).length);
-        // State Abbr add
+
+        // Circle text add
         chart.selectAll('g')
-            .data(data)
+            .data(keysList)
             .enter()
             .append("text")
-            .attr("x", function(data, index) { return ((index+1)/(Object.keys(jsonData.keys()).length+1) * svgWidth); })
-            .attr("y", function(data) { return svgHeight/4 + 5; })
-            .text(function (data) { return data.keys().toUpperCase(); })
+            .attr("x", function(data, index) { 
+                return ((index+1)/(keysList.length+1)) * svgWidth; 
+            })
+            .attr("y", function(data, index) { 
+                return (svgHeight/2)+(-100*(valueList[index]/1))+5; 
+            })
+            .text(function (data) { return data.toUpperCase(); })
             .attr('fill', 'white')
-            .style("font-size", function(data) { return '22'; })
+            .style("font-size", function(data) { return '12'; })
             .style("text-anchor", "middle");
     });
 };
