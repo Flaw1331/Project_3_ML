a919a7aac6769b5d283eb73f43ca5285b02b8f6f
Blaine Anspaugh <spaugh56@yahoo.com>
2018-06-08 22:58:38 -0500

    fixed upload button
    fixed upload button

--- Testing_Audio/app.py
+++ Testing_Audio/app.py
@@ -45,6 +45,15 @@ def home():
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    folder = './test_folder'
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+        #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+    except Exception as e:
+        print(e)
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
